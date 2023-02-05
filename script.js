4c53a3dc6c76dd6c97bedd20fab780ac1eacb198
claudio-py <claudio.3777@gmail.com>
2023-02-04 22:49:32 -0300

    selection sound effects

--- script.js
+++ script.js
@@ -7,3 +7,37 @@ function toggleMode() {
   // html.classList.add("light") }
   html.classList.toggle("light")
 }
+
+var playBtn = document.getElementById("play"),
+  hearbeat = document.getElementById("button-hover")
+audios = document.querySelectorAll("audio")
+
+console.log(audios)
+
+playBtn.addEventListener(
+  "mouseover",
+  function () {
+    ;[].forEach.call(audios, function (audio) {
+      // do whatever
+      audio.play()
+    })
+  },
+  false
+)
+
+playBtn.addEventListener(
+  "mouseleave",
+  function () {
+    heartbeat.pause()
+    heartbeat.currentTime = 0
+  },
+  false
+)
+const audio = new Audio("./assets/selection.mp3");
+const buttons = document.querySelectorAll("li");
+
+buttons.forEach((li) => {
+  li.addEventListener("click", () => {
+    audio.play()
+  })
+})
